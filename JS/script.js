78c6d66102b711852cb868a4113f9755ec74f4a4
Ba6aka <bam6ykba6aka@gmail.com>
2022-11-23 13:23:32 +0200

    add switch case and while cycle

--- JS/script.js
+++ JS/script.js
@@ -4,21 +4,40 @@ const answerCountFilm = 'How many film do you watch?',
     answerFilmName = 'what is the name of the last movie',
     answerFilmRate = 'how would you rate it';
 
-const numberOfFilms = prompt(answerCountFilm ,''),
-    a = prompt(answerFilmName, ''),
-    b = prompt(answerFilmRate,''),
-    c = prompt(answerFilmName, ''),
-    d = prompt(answerFilmRate,'');
+const numberOfFilms = prompt(answerCountFilm ,'');
+    
+
 
 const personalMovieBD = {
-    count: numberOfFilms,
+    conunt: numberOfFilms,
     movies:{},
     actors:{},
     genres:[],
     privat:false
 };
 
-personalMovieBD.movies[a] = b;
-personalMovieBD.movies[c] = d;
 
-console.log(personalMovieBD);
+
+switch(true){
+    case ((personalMovieBD.conunt < 10) && (personalMovieBD.conunt > 0)):
+        alert('You have not seen a lot of films');
+        break;
+    case ((personalMovieBD.conunt < 20) && (personalMovieBD.conunt > 10)):
+        alert('You clacial viewer');
+        break;
+    case ((personalMovieBD.conunt > 30)):
+        alert('You is real cinophile');
+        break;
+    default:
+        alert('something is not ok')
+};
+
+while (2 > Object.keys(personalMovieBD.movies).length){
+    const a = prompt(answerFilmName, ''),
+        b = prompt(answerFilmRate,'');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50){
+        personalMovieBD.movies[a] = b;
+    }    
+}
+
