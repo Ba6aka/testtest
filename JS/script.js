c6556f4580f8955c6e1dce897a9e9f87ff983bff
Ba6aka <bam6ykba6aka@gmail.com>
2022-12-20 18:28:29 +0200

    add functions to personalMovieBD

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-"Use strict"
+"Use strict";
 
 const answerCountFilm = 'How many film do you watch?',
     answerFilmName = 'what is the name of the last movie',
@@ -8,67 +8,84 @@ const answerCountFilm = 'How many film do you watch?',
 let numberOfFilms;
 let genreFilms;
 
-function countOfFilms() {
-    numberOfFilms = prompt(answerCountFilm ,'');
-    while (numberOfFilms == '' || numberOfFilms == null ||isNaN(numberOfFilms)){
-            numberOfFilms = prompt(answerCountFilm ,'');
-    }
-}
+
 const personalMovieBD = {
-    conunt: numberOfFilms,
     movies:{},
     actors:{},
     genres:[],
     privat:false,
-    genre:[]
-};
+    genre:[],
+    countOfFilms:function () {
+        numberOfFilms = prompt(answerCountFilm ,'');
+        while (numberOfFilms == '' || numberOfFilms == null ||isNaN(numberOfFilms)){
+                numberOfFilms = prompt(answerCountFilm ,'');
+        }
+        return numberOfFilms;
+    },
+    writeYourGenre:function () {
+        for(let i = 0; i < 3; i++){
+            genreFilms = prompt(`${answerMovieGenre} ${i+1}` ,'');
+            if (genreFilms != null && genreFilms != '' && genreFilms.length < 50 ){
+                this.genre[i] = genreFilms;   
+            }
+            else{
+                i--;
+            }
+            
+    }
+            console.log(personalMovieBD.genre);},
 
-function writeYourGenre() {
-    for(let i = 0; i < 3; i++){
-        genreFilms = prompt(`${answerMovieGenre} ${i+1}` ,'');
-        personalMovieBD.genre[i] = genreFilms;
-        
-}
-        console.log(  personalMovieBD.genre);}
-
-writeYourGenre();
-
-
-
-const c = personalMovieBD.conunt;
-
-function detectPersonalLevel(){
-
-    switch(true){
-        case (( c < 10) && (c > 0)):
-            alert('You have not seen a lot of films');
-            break;
-        case ((c < 20) && (c > 10)):
-            alert('You clacial viewer');
-            break;
-        case ((c > 30)):
-            alert('You is real cinophile');
-            break;
-        default:
-            alert('something is not ok')
-    };
-}
-
-
-
-function rememberMyFilms(){
-    while (2 > Object.keys(personalMovieBD.movies).length){
-        const a = prompt(answerFilmName, ''),
-            b = prompt(answerFilmRate,'');
-        if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50){
-            personalMovieBD.movies[a] = b;
+    detectPersonalLevel:function (){
+        const c = this.countOfFilms();
+        switch(true){
+            case (( c < 10) && (c > 0)):
+                alert('You have not seen a lot of films');
+                break;
+            case ((c < 20) && (c > 10)):
+                alert('You clacial viewer');
+                break;
+            case ((c > 30)):
+                alert('You is real cinophile');
+                break;
+            default:
+                alert('something is not ok');
+        }
+    },
+    rememberMyFilms:function (){
+        while (2 > Object.keys(personalMovieBD.movies).length){
+            const a = prompt(answerFilmName, ''),
+                b = prompt(answerFilmRate,'');
+            if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50){
+                personalMovieBD.movies[a] = b;
+            }
+        }
+        return personalMovieBD;      
+    },
+    toggleVisibleMyBD: function(){
+        if (this.privat === false){
+            this.privat = true;
+        }
+        else {
+            this.privat = false;
+        } 
+    },
+    showMyBD:function () {
+        if (this.privat === false){
+            this.genre.forEach(function(item, i){
+                console.log(`Любимый жанр ${i+1} - это ${item}`);
+            });
         }
     }
-    return personalMovieBD;      
-}
 
-function showMyBD() {
-    if (personalMovieBD.privat === false){
-        console.log(personalMovieBD.movies)
-    }
-}
+};
+
+
+personalMovieBD.writeYourGenre();
+
+
+
+
+
+
+
+
